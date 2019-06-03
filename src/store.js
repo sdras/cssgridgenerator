71bbfc4bea3bc505c2f895496c7ef9e937ca1587
trickstival <trick_stival@hotmail.com>
2019-06-02 21:30:41 -0300

    Added css repeat support

--- src/store.js
+++ src/store.js
@@ -2,6 +2,28 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
+const groupRepeatedUnits = (templateArray = ['1fr']) => {
+  const groups = [[templateArray.shift()]]
+  for (const templateUnit of templateArray) {
+    const lastGroup = groups[groups.length - 1]
+    if (lastGroup.indexOf(templateUnit) !== -1) {
+      lastGroup.push(templateUnit)
+      continue
+    }
+    groups.push([templateUnit])
+  }
+  return groups
+}
+
+const createRepetition = (groups, maxRepetition = 1) => {
+  return groups.map(group =>
+    // If you want to add repetition only when a measure is repeated more than 1 time,
+    // change maxRepetition value
+    group.length === maxRepetition
+      ? group.join(' ')
+      : `repeat(${group.length}, ${group[0]})`
+  ).join(' ')
+}
 
 export default new Vuex.Store({
   state: {
@@ -15,10 +37,16 @@ export default new Vuex.Store({
   },
   getters: {
     colTemplate(state) {
-      return state.colArr.map(i => i["unit"]).join(" ");
+      // Maybe this mapping can be executed inside groupRepeatedUnits
+      // to avoid code repetition
+      const templateArray = state.colArr.map(i => i["unit"]);
+      const unitGroups = groupRepeatedUnits(templateArray);
+      return createRepetition(unitGroups);
     },
     rowTemplate(state) {
-      return state.rowArr.map(i => i["unit"]).join(" ");
+      const templateArray = state.rowArr.map(i => i["unit"]);
+      const unitGroups = groupRepeatedUnits(templateArray);
+      return createRepetition(unitGroups);
     },
     divNum(state) {
       return state.columns * state.rows;
