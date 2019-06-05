8462401f9439876f361a20be006d8eaceab912c9
Benjamin Konopka <benjamin.konopka@virtual-identity.com>
2019-06-05 18:49:05 +0200

    add german translation in de.json

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,18 +2,20 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import en from './en.json'
 import es from './es.json'
+import de from './de.json'
 
 Vue.use(VueI18n)
 
-const locale = window.navigator.language ? window.navigator.language.substring(0, 2) : "en";
+const locale = window.navigator.language ? window.navigator.language.substring(0, 2) : "de";
 
 export function createI18n () {
   return new VueI18n({
     locale: locale,
-    fallbackLocale: 'en',
+    fallbackLocale: 'de',
     messages: {
       'en': en,
-      'es': es
+      'es': es,
+      'de': de
     }
   })
 };
