b8f979393ff14a38e1ec84b184d5d4d5f63f9e19
Sarah Drasner <sarah.drasner@gmail.com>
2019-06-23 15:36:30 -0600

    fix spacing for copy/paste

--- src/components/AppCode.vue
+++ src/components/AppCode.vue
@@ -17,37 +17,44 @@
       <div id="code" ref="code">
         <p>
           <span class="cname">.parent</span> {
+          <br>
+          <span class="sp">
+            <span class="ckey">display</span>:
+            <span class="cprop">grid</span>;
+          </span>
+          <br>
+          <span class="sp">
+            <span class="ckey">grid-template-columns</span>:
+            <span class="cprop">{{ colTemplate }}</span>;
+          </span>
+          <br>
+          <span class="sp">
+            <span class="ckey">grid-template-rows</span>:
+            <span class="cprop">{{ rowTemplate }}</span>;
+          </span>
+          <br>
+          <span class="sp">
+            <span class="ckey">grid-column-gap</span>:
+            <span class="cprop">{{ columngap }}px;</span>
+          </span>
+          <br>
+          <span class="sp">
+            <span class="ckey">grid-row-gap</span>:
+            <span class="cprop">{{ rowgap }}px</span>;
+          </span>
+          <br>
+          <span v-if="childarea.length > 0" class="child">
+            <span v-for="(child, i) in childarea" :key="child">
+              <span>
+                <span class="cname">.div{{ i + 1 }}</span> {
+                <span class="ckey">grid-area</span>:
+                <span class="cprop">{{ child }}</span>; }
+              </span>
+              <br>
+            </span>
+          </span>
+          }
         </p>
-        <p class="sp">
-          <span class="ckey">display</span>:
-          <span class="cprop">grid</span>;
-        </p>
-        <p class="sp">
-          <span class="ckey">grid-template-columns</span>:
-          <span class="cprop">{{ colTemplate }}</span>;
-        </p>
-        <p class="sp">
-          <span class="ckey">grid-template-rows</span>:
-          <span class="cprop">{{ rowTemplate }}</span>;
-        </p>
-        <p class="sp">
-          <span class="ckey">grid-column-gap</span>:
-          <span class="cprop">{{ columngap }}px;</span>
-        </p>
-        <p class="sp">
-          <span class="ckey">grid-row-gap</span>:
-          <span class="cprop">{{ rowgap }}px</span>;
-        </p>
-        <div v-if="childarea.length > 0" class="child">
-          <div v-for="(child, i) in childarea" :key="child">
-            <p>
-              <span class="cname">.div{{ i + 1 }}</span> {
-              <span class="ckey">grid-area</span>:
-              <span class="cprop">{{ child }}</span>; }
-            </p>
-          </div>
-        </div>
-        <p>}</p>
       </div>
       <!--code-->
     </div>
