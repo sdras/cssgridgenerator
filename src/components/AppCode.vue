6c6b2c768f3757ff32f93096466fbad2e0d859c6
Mike <mike@wlvs.tv>
2019-10-04 21:54:17 -0400

    Adding CSS type selector and IE compatibility mode Added a dropdown to select CSS styles, default is vanilla CSS. You can also select SCSS (allows nested children) and IE compatibility mode. Added the CSS changes required for ms grid, the IE compatible implementation of css grid

--- src/components/AppCode.vue
+++ src/components/AppCode.vue
@@ -34,7 +34,7 @@
         <div v-if="showHtml">
           <p>
             &lt;<span class="cname">div</span>
-            <span class="cprop">class="parent"</span>&gt;
+            <span class="cprop">&nbsp;class="parent"</span>&gt;
             <br>
             <span v-if="childarea.length > 0">
               <span v-for="(child, i) in childarea" :key="child">
@@ -56,16 +56,37 @@
           <p>
             <span class="cname">.parent</span> {
             <br>
+            <template v-if="cssType==='iec'">
+              <span class="sp">
+                <span class="ckey">display</span>:
+                <span class="cprop">ms-grid</span>;
+              </span>
+              <br>
+            </template>
             <span class="sp">
               <span class="ckey">display</span>:
               <span class="cprop">grid</span>;
             </span>
             <br>
+             <template v-if="cssType==='iec'">
+              <span class="sp">
+                <span class="ckey">-ms-grid-columns</span>:
+                <span class="cprop">{{ msColTemplate }}</span>;
+              </span>
+              <br>
+            </template>
             <span class="sp">
               <span class="ckey">grid-template-columns</span>:
               <span class="cprop">{{ colTemplate }}</span>;
             </span>
             <br>
+             <template v-if="cssType==='iec'">
+              <span class="sp">
+                <span class="ckey">-ms-grid-rows</span>:
+                <span class="cprop">{{ msRowTemplate }}</span>;
+              </span>
+              <br>
+            </template>
             <span class="sp">
               <span class="ckey">grid-template-rows</span>:
               <span class="cprop">{{ rowTemplate }}</span>;
@@ -76,25 +97,62 @@
               <span class="cprop">{{ columngap }}px;</span>
             </span>
             <br>
+            <template v-if="cssType==='iec'">
+              <span class="sp">
+                <span class="ckey">margin-right</span>:
+                <span class="cprop">{{ msRowTemplate }}</span>;
+              </span>
+              <br>
+            </template>
             <span class="sp">
               <span class="ckey">grid-row-gap</span>:
               <span class="cprop">{{ rowgap }}px</span>;
             </span> 
             <br>
-            <span>}</span>
+            <span v-if="cssType==='css' || cssType ==='iec'">}</span>
           </p>
-          <p>
-            <span v-if="childarea.length > 0">
-              <span v-for="(child, i) in childarea" :key="child">
-                <span>
-                  <span class="cname">.div{{ i + 1 }}</span> {
-                  <span class="ckey">grid-area</span>:
-                  <span class="cprop">{{ child }}</span>; }
+          <p :class="{child : cssType==='scss'}">
+            <template v-if="cssType==='iec'">
+              <span v-if="mschildarea.length > 0">
+                <span v-for="(child, i) in mschildarea" :key="child.cString">
+                    <span class="cname">.div{{ i + 1 }}</span> {
+                    <br>
+                    <span class="ckey msc">-ms-grid-row</span>:
+                    <span class="cprop">{{ child.startRow }}</span>;&#9;
+                    <span class="ckey">-ms-grid-row-span</span>:
+                    <span class="cprop">{{ calculateMsSpan(child.startRow, child.endRow) }}</span>; 
+                    <br>
+                    <template v-if="rowgap > 0 || columngap > 0">
+                      <span class="ckey msc">margin</span>:
+                      <span class="cprop">{{ calculateMsMargin(columngap, rowgap) }}</span>;
+                      <br>
+                    </template>
+                    <span class="ckey msc">-ms-grid-column</span>:
+                    <span class="cprop">{{ child.startCol }}</span>; 
+                    <span class="ckey">-ms-grid-column-span</span>:
+                    <span class="cprop">{{ calculateMsSpan(child.startCol, child.endCol)  }}</span>; 
+                    <br>
+                    <span class="ckey msc">grid-area</span>:           
+                    <span class="cprop">{{ child.cString }}</span>; 
+                    <br>}
+                    <br>
                 </span>
-                <br>
               </span>
-            </span>
+            </template>
+            <template v-else>
+              <span v-if="childarea.length > 0">
+                <span v-for="(child, i) in childarea" :key="child">
+                  <span>
+                    <span class="cname">.div{{ i + 1 }}</span> {
+                    <span class="ckey">grid-area</span>:
+                    <span class="cprop">{{ child }}</span>; }
+                  </span>
+                  <br>
+                </span>
+              </span>
+            </template>
           </p>
+          <span v-if="cssType==='scss'">}</span>
         </div>
       </div>
       <!--code-->
@@ -114,8 +172,8 @@ export default {
     };
   },
   computed: {
-    ...mapState(["columngap", "rowgap", "childarea"]),
-    ...mapGetters(["rowTemplate", "colTemplate"])
+    ...mapState(["columngap", "rowgap", "childarea", "mschildarea"]),
+    ...mapGetters(["rowTemplate", "colTemplate", "msColTemplate", "msRowTemplate"])
   },
   methods: {
     copy() {
@@ -146,6 +204,20 @@ export default {
     },
     toggleHtml() {
       this.showHtml = !this.showHtml;
+    },
+    calculateMsMargin(colgap, rowgap){
+      let bottom = rowgap > 0 ? `${rowgap}px` : 0,
+          right = colgap > 0 ? `${colgap}px` : 0;
+      
+      return `0 ${right} ${bottom} 0`;
+    },
+    calculateMsSpan(start, end){
+      let initialSpan = end - start;
+      if( initialSpan <= 1){
+        return 1;
+      } else {
+        return initialSpan;
+      }
     }
   }
 };
@@ -213,7 +285,11 @@ export default {
 }
 
 .child {
-  padding-left: 40px;
+  padding-left: 20px;
+}
+
+.msc {
+  padding-left:20px;
 }
 
 .codegroup {
