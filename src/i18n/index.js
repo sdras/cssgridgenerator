f03089bc5472b6af5bd9a3034263bc113be5d812
FlandreDaisuki <vbnm123c+github@gmail.com>
2020-10-16 11:13:52 +0800

    Add zh-TW locale

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -5,21 +5,34 @@ import es from './es.json'
 import zh from './zh.json'
 import pt from './pt.json'
 import fr from './fr.json'
+import zhTW from './zh-TW.json'
 
 Vue.use(VueI18n)
 
-const locale = window.navigator.language ? window.navigator.language.substring(0, 2) : "en";
+const messages = {
+  'en': en,
+  'es': es,
+  'zh': zh,
+  'pt': pt,
+  'fr': fr,
+  'zh-TW': zhTW,
+};
+
+const locale = (() => {
+  const locales = Object.keys(messages);
+  const lang = window.navigator.language;
+  if (!lang) { return 'en'; }
+  if (locales.includes(lang)) { return lang; }
+
+  const langShort = lang.slice(0, 2);
+  if (locales.includes(langShort)) { return langShort; }
+  return 'en';
+})();
 
 export function createI18n () {
   return new VueI18n({
     locale: locale,
     fallbackLocale: 'en',
-    messages: {
-      'en': en,
-      'es': es,
-      'zh': zh,
-      'pt': pt,
-      'fr': fr
-    }
-  })
+    messages,
+  });
 };
